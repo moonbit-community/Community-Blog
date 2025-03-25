bdd55c04634a447b9be157a7053376c05a2fb469
东灯 <me@lampese.com>
2025-03-26 00:54:25 +0800

    feat: add vite watch

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import chokidar from "chokidar";
+import cp from "node:child_process";
+import fs from "node:fs";
+
+/**
+ * @type {import('vite').Plugin}
+ */
+const plugin = {
+  buildStart() {
+    function build() {
+      fs.rmSync("trees/publish", { recursive: true, force: true });
+      fs.rmSync("trees/trees", { recursive: true, force: true });
+      cp.execSync("kodama c -r trees");
+      cp.execSync("node scripts/highlight.js");
+    }
+
+    build();
+
+    chokidar.watch(["trees"], { ignoreInitial: true }).on("all", (_, path) => {
+      if (
+        !(path.startsWith("trees/trees") || path.startsWith("trees/publish"))
+      ) {
+        build();
+      }
+    });
+  },
+};
+
+export default defineConfig({
+  root: "./trees/publish",
+  plugins: [plugin],
+});
