f8c0f924e483fead98fe6f900e4969c074fcc000
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-16 08:12:25 +0800

    fix: vite script watch options

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,8 @@
 import { defineConfig } from "vite";
-import chokidar from "chokidar";
-import cp from "node:child_process";
-import fs from "node:fs";
 
-/**
- * @type {import('vite').Plugin}
- */
-const plugin = {
-  buildStart() {
-    function build() {
-      fs.rmSync("trees/publish", { recursive: true, force: true });
-      fs.rmSync("trees/trees", { recursive: true, force: true });
-      cp.execSync("kodama c -r trees");
-      cp.execSync("node scripts/highlight.js");
-    }
-
-    build();
-
-    chokidar
-      .watch(["trees"], { ignored: /^trees\/(trees|publish)/ })
-      .on("all", (_, path) => {
-        build();
-      });
-  },
-};
-
+// /**
+//  * @type {import('vite').Plugin}
+//  */
 export default defineConfig({
   root: "./trees/publish",
-  plugins: [plugin],
 });
---
+++ watch.js
@@ -0,0 +1,22 @@
+import chokidar from "chokidar";
+import cp from "node:child_process";
+import fs from "node:fs";
+
+let watched_path = fs.readdirSync("trees", {
+  withFileTypes: true
+}).filter((dir) => {
+  return dir.name !== 'publish' && dir.name !== 'trees'
+}).map((dir) => {
+  return `trees/${dir.name}`
+})
+
+function build() {
+  cp.execSync("kodama c -r trees");
+  cp.execSync("node scripts/highlight.js");
+}
+build();
+
+chokidar.watch(watched_path, { ignoreInitial: true }).on("all", (_, path) => {
+  console.log(path)
+  build()
+});
