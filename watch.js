255b55e69f48b681f0c7efaa0171712975cace69
东灯 <me@lampese.com>
2025-04-16 19:35:08 +0800

    scripts: perf preview scripts

--- watch.js
+++ watch.js
@@ -2,13 +2,12 @@ import chokidar from "chokidar";
 import cp from "node:child_process";
 import fs from "node:fs";
 
-let watched_path = fs.readdirSync("trees", {
-  withFileTypes: true
-}).filter((dir) => {
-  return dir.name !== 'publish' && dir.name !== 'trees'
-}).map((dir) => {
-  return `trees/${dir.name}`
-})
+let watched_path = fs
+  .readdirSync("trees", {
+    withFileTypes: true,
+  })
+  .filter((dir) => dir.name !== "publish" && dir.name !== "trees")
+  .map((dir) => `trees/${dir.name}`);
 
 function build() {
   cp.execSync("kodama c -r trees");
@@ -17,6 +16,6 @@ function build() {
 build();
 
 chokidar.watch(watched_path, { ignoreInitial: true }).on("all", (_, path) => {
-  console.log(path)
-  build()
+  console.log(path);
+  build();
 });
