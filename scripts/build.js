d7072e87d2f980b68cf4d68b0ef823db5d22286f
CAIMEO <38813005+CAIMEOX@users.noreply.github.com>
2025-10-21 10:53:58 +0800

    fix file system problem

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ import cp from "node:child_process";
 function build() {
   fs.cpSync("trees", "__trees", { recursive: true });
   cp.execSync("node scripts/index.js literal __trees");
-  cp.execSync("kodama build");
+  cp.execSync("kodama build --config kodama.toml");
   cp.execSync("node scripts/index.js highlight __trees");
   fs.rmSync("publish", { recursive: true, force: true });
   fs.cpSync("__trees/publish", "publish", { recursive: true });
